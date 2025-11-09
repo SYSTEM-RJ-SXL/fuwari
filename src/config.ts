b835812925db70357a93a41997fb714194a3b37a
RTXVDZ <334063894@qq.com>
2025-11-09 21:12:56 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -17,10 +17,10 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "https://res-cn.rtxvdz.top/banner.png, // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "assets/images/demo-banner.png, // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
-			enable: false, // Display the credit text of the banner image
+			enable: true, // Display the credit text of the banner image
 			text: "", // Credit text to be displayed
 			url: "", // (Optional) URL link to the original artwork or artist's page
 		},
