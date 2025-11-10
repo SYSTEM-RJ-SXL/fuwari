d29a7c66cd71a8f5f4e4d26089a032894bf61f91
RTXVDZ <334063894@qq.com>
2025-11-10 20:50:09 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -17,12 +17,12 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "https://get.2sb.org/https://res-cdn.rtxvdz.top/banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "https://lz.qaiu.top/parser?url=https://www.lanzout.com/iFvB63aq595e&pwd=6666", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: true, // Display the credit text of the banner image
 			text: "rtxvdz", // Credit text to be displayed
-			url: "https://get.2sb.org/https://res-cdn.rtxvdz.top/banner.png", // (Optional) URL link to the original artwork or artist's page
+			url: "https://lz.qaiu.top/parser?url=https://www.lanzout.com/iFvB63aq595e&pwd=6666", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
 	toc: {
