2755d94067447580286f9c747c892a7ba908a308
RTXVDZ <334063894@qq.com>
2025-11-10 20:56:15 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -12,8 +12,8 @@ export const siteConfig: SiteConfig = {
 	subtitle: "b站科技区大肘子",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
-		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: false, // Hide the theme color picker for visitors
+		hue: 255, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		fixed: true, // Hide the theme color picker for visitors
 	},
 	banner: {
 		enable: true,
