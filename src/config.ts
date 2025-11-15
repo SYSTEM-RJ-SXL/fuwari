7ae0e2b76d0c0872eb487d7ec9b6089fa7ab035d
RTXVDZ <334063894@qq.com>
2025-11-15 15:39:25 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "rtxvdz",
-	subtitle: "b站科技区大肘子",
+	subtitle: "blog",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 255, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -56,7 +56,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "/favicon/favicon-light-192.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "rtxvdz",
-	bio: "b站rtxvdz，科技区大肘子",
+	bio: "成功的办法不止一种",
 	links: [
 		{
 			name: "bilibili",
