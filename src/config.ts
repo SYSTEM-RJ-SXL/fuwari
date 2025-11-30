e76c471f488f8d06036b689edc5eeb258beadb24
rtxvdz <334063894@qq.com>
2025-11-30 14:05:48 +0800

    启用主题夜色固定和强制黑色

--- src/config.ts
+++ src/config.ts
@@ -13,8 +13,8 @@ export const siteConfig: SiteConfig = {
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 275, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: false, // Hide the theme color picker for visitors
-	//	forceDarkMode: true,
+		fixed: true, // Hide the theme color picker for visitors
+		forceDarkMode: true,
 	},
 	banner: {
 		enable: false,
